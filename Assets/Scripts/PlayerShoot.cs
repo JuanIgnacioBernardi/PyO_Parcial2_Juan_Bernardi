1d14c3837c7da408c930dec09dbccf4614c2e7b1
KronoEE <juanibernardi019@gmail.com>
2026-07-12 04:21:21 -0300

    Added player shoot behavior to player

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+public class PlayerShoot : MonoBehaviour
+{
+    [Header("Shooting Settings")]
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private ProjectilePool projectilePool;
+    [SerializeField] private float baseDamage = 1f;
+    [SerializeField] private float bulletSpeed = 10f;
+    [SerializeField] private float fireCooldown = 1f;
+
+    private float cooldownTimer;
+    public float DamageMultiplier { get; set; } = 1f;
+
+    private void Update()
+    {
+        cooldownTimer -= Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space) && cooldownTimer <= 0f)
+        {
+            Shoot();
+            cooldownTimer = fireCooldown;
+        }
+    }
+    private void Shoot()
+    {
+        projectilePool.Spawn(firePoint.position, Vector2.up, bulletSpeed, baseDamage * DamageMultiplier, ProjectileOwner.Player);
+    }
+}
