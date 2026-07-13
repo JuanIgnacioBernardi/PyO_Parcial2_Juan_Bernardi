52a6e6bdbc224c7ee0d417a586f6e874b8ba981d
KronoEE <juanibernardi019@gmail.com>
2026-07-13 18:19:40 -0300

    strategy decides when turn on/off laser beam

--- Assets/Scripts/Enemy/AttackTypes/VerticalLaserStrategy.cs
+++ Assets/Scripts/Enemy/AttackTypes/VerticalLaserStrategy.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
-
-public class VerticalLaserStrategy : MonoBehaviour
+public class VerticalLaserStrategy : MonoBehaviour, IEnemyAttackStrategy
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Laser Settings")]
+    [SerializeField] private EnemyAttackDataSO data;
+    [SerializeField] private LaserBeam laserBeam;
+    [SerializeField] private float laserDuration = 2f;
 
-    // Update is called once per frame
-    void Update()
+    private float timer;
+    private bool laserActive;
+    public void Init(Transform origin, ProjectilePool projectilePool)
     {
-        
+        laserBeam.SetDamage(data.damage);
+        laserBeam.gameObject.SetActive(false);
+        timer = data.fireInterval;
+    }
+    public void Tick(float deltaTime)
+    {
+        timer -= deltaTime;
+        if (timer > 0f) return;
+
+        if (!laserActive)
+        {
+            laserActive = true;
+            laserBeam.gameObject.SetActive(true);
+            timer = laserDuration;
+        }
+        else
+        {
+            laserActive = false;
+            laserBeam.gameObject.SetActive(false);
+            timer = data.fireInterval;
+        }
     }
 }
