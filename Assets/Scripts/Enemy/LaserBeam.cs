d4f611245b89330eed9d18b8a59bdadc084b647e
KronoEE <juanibernardi019@gmail.com>
2026-07-13 18:53:12 -0300

    Now laserbeam draw a raycast below to check player

--- Assets/Scripts/Enemy/LaserBeam.cs
+++ Assets/Scripts/Enemy/LaserBeam.cs
@@ -1,17 +1,52 @@
 using UnityEngine;
-[RequireComponent(typeof(Collider2D))]
+
+[RequireComponent(typeof(LineRenderer))]
 public class LaserBeam : MonoBehaviour
 {
+    [Header("Laser Settings")]
+    [SerializeField] private float maxLength = 20f;
+
+    private LineRenderer line;
     private float damage;
+    private bool hasDamagedThisActivation;
+    private void Awake()
+    {
+        line = GetComponent<LineRenderer>();
+    }
     public void SetDamage(float value)
     {
         damage = value;
     }
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnEnable()
     {
-        if (!other.CompareTag("Player")) return;
+        hasDamagedThisActivation = false;
+    }
+    private void Update()
+    {
+        RaycastHit2D playerHit = FindPlayerHit();
+        float length = playerHit.collider != null ? playerHit.distance : maxLength;
 
-        IDamageable damageable = other.GetComponent<IDamageable>();
-        damageable?.TakeDamage(damage);
+        line.SetPosition(0, Vector3.zero);
+        line.SetPosition(1, new Vector3(0f, -length, 0f));
+
+        if (playerHit.collider != null && !hasDamagedThisActivation)
+        {
+            IDamageable damageable = playerHit.collider.GetComponent<IDamageable>();
+            if (damageable != null)
+            {
+                damageable.TakeDamage(damage);
+                hasDamagedThisActivation = true;
+            }
+        }
+    }
+    private RaycastHit2D FindPlayerHit()
+    {
+        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, Vector2.down, maxLength);
+        foreach (RaycastHit2D hit in hits)
+        {
+            if (hit.collider != null && hit.collider.CompareTag("Player"))
+                return hit;
+        }
+        return default;
     }
 }
