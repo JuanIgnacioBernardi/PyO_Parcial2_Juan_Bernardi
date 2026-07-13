935c15ed9122e0fdffd889a0d19e569765e6b5f4
KronoEE <juanibernardi019@gmail.com>
2026-07-13 20:11:47 -0300

    Update game manager to reset game after x seconds

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 public class GameManager : MonoBehaviour
 {
     [Header("Game Settings")]
     [SerializeField] private HealthSystem playerHealth;
-    [SerializeField] private Enemy[] enemies; 
+    [SerializeField] private Enemy[] enemies;
+    [SerializeField] private float restartDelay = 2f;
 
     private int remainingEnemies;
+    private bool gameEnded;
     private void Awake()
     {
         remainingEnemies = enemies.Length;
@@ -24,14 +28,25 @@ public class GameManager : MonoBehaviour
     {
         remainingEnemies--;
         if (remainingEnemies <= 0)
-        {
-            Debug.Log("VICTORYY!");
-            Time.timeScale = 0f;
-        }
+            EndGame("VICTORY: ALL ENEMIES ARE DEFEATED!");
     }
     private void HandlePlayerDied()
     {
-        Debug.Log("GAME OVER!");
+        EndGame("GAME OVER: player died.");
+    }
+    private void EndGame(string message)
+    {
+        if (gameEnded) return;
+        gameEnded = true;
+
+        Debug.Log(message);
+        StartCoroutine(RestartAfterDelay());
+    }
+    private IEnumerator RestartAfterDelay()
+    {
         Time.timeScale = 0f;
+        yield return new WaitForSecondsRealtime(restartDelay);
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
