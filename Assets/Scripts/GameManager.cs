d3fed89417b91210442530fa463c4f5a3bcf8232
KronoEE <juanibernardi019@gmail.com>
2026-07-13 18:45:32 -0300

    Game manager now handle win & lose events

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,37 @@
 using UnityEngine;
-
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Game Settings")]
+    [SerializeField] private HealthSystem playerHealth;
+    [SerializeField] private Enemy[] enemies; 
 
-    // Update is called once per frame
-    void Update()
+    private int remainingEnemies;
+    private void Awake()
     {
-        
+        remainingEnemies = enemies.Length;
+    }
+    private void OnEnable()
+    {
+        Enemy.OnAnyEnemyDied += HandleEnemyDied;
+        playerHealth.onDie += HandlePlayerDied;
+    }
+    private void OnDisable()
+    {
+        Enemy.OnAnyEnemyDied -= HandleEnemyDied;
+        playerHealth.onDie -= HandlePlayerDied;
+    }
+    private void HandleEnemyDied(Enemy enemy)
+    {
+        remainingEnemies--;
+        if (remainingEnemies <= 0)
+        {
+            Debug.Log("VICTORYY!");
+            Time.timeScale = 0f;
+        }
+    }
+    private void HandlePlayerDied()
+    {
+        Debug.Log("GAME OVER!");
+        Time.timeScale = 0f;
     }
 }
